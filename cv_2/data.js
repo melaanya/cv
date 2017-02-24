4741bd127f60b55b35477d5bb97fac7ea2f33c15
melaanya <ankaberg@yandex.ru>
2017-02-24 10:35:29 +0300

    task 2-1, 2-2 finished, 2-3 started

--- cv_2/data.js
+++ cv_2/data.js
@@ -1,6 +1,5 @@
 var background = "#FFE5B4";
 
-var example_pixel = [0, 0];
 var img_width = 0; 
 var img_height = 0;
 
@@ -56,8 +55,93 @@ function handleFileSelect(event, name)
 }
 
 function getPixelCanvas(e) {
+	var canvas = document.getElementById("canvas_correct");
     example_pixel = [e.pageX - this.offsetLeft, e.pageY - this.offsetTop];
-    console.log(example_pixel);
+
+	data_full_correct = getData("canvas_correct");
+	data_full_spoiled = getData("canvas_spoiled");
+
+	data_correct = data_full_correct.data;
+	data_spoiled = data_full_spoiled.data;
+
+    var cur_pos = (example_pixel[0] * img_height + example_pixel[1]) * 4;
+
+	r_dst = data_correct[cur_pos];
+	r_src = data_spoiled[cur_pos];
+	coeff_r = r_dst / r_src;
+
+	g_dst = data_correct[cur_pos + 1];
+	g_src = data_spoiled[cur_pos + 1];
+	coeff_g = g_dst / g_src;
+
+	b_dst = data_correct[cur_pos + 2];
+	b_src = data_spoiled[cur_pos + 2];
+	coeff_b = b_dst / b_src;
+
+	for (var i = 0; i < img_width; ++i) {
+		for (var j = 0; j < img_height; ++j) {
+			cur = (i * img_height + j) * 4;
+			data_spoiled[cur] *= coeff_r;
+			data_spoiled[cur + 1] *= coeff_g;
+			data_spoiled[cur + 2] *= coeff_b;
+		}
+	}
+	
+	canvas = document.getElementById("canvas_spoiled");
+	ctx = canvas.getContext('2d');
+	ctx.putImageData(data_full_spoiled, 0, 0);
+}
+
+function greyWorld() {
+	var canvas = document.getElementById("canvas_spoiled");
+	var data_full = getData("canvas_spoiled");
+	var data = data_full.data;
+
+	var rgb_avg = [0, 0, 0];
+
+	for (var i = 0; i < img_width; ++i) {
+		for (var j = 0; j < img_height; ++j) {
+			cur = (i * img_height + j) * 4;
+			rgb_avg[0] += data[cur];
+			rgb_avg[1] += data[cur + 1];
+			rgb_avg[2] += data[cur + 2];
+		}
+	}
+
+	var avg = (rgb_avg[0] + rgb_avg[1]+ rgb_avg[2]) / 3 / data.length;
+	rgb_avg = [avg / rgb_avg[0] * data.length, avg / rgb_avg[1] * data.length, avg / rgb_avg[2] * data.length];
+
+	for (var i = 0; i < img_width; ++i) {
+		for (var j = 0; j < img_height; ++j) {
+			cur = (i * img_height + j) * 4;
+			data[cur] *= rgb_avg[0];
+			data[cur + 1] *= rgb_avg[1];
+			data[cur + 2] *= rgb_avg[2];
+		}
+	}
+
+	ctx = canvas.getContext('2d');
+	ctx.putImageData(data_full, 0, 0);
+}
+
+function brightnessChange(data, fun) {
+
+	for (var i = 0; i < img_width; ++i) {
+		for (var j = 0; j < img_height; ++j) {
+			cur = (i * img_height + j) * 4;
+			var b = Math.max(data[cur], data[cur + 1], data[cur + 2]); 
+			var num_b = 0;
+		}
+	}
+}
+
+function funcTransform() {
+	// brightness - max of rgb
+	var sel = document.getElementById("functionSelect");
+	var curFun = parseInt(sel.options[sel.selectedIndex].value);
+	console.log(curFun);
+
+
 }
 
 function getData(name) {
@@ -68,41 +152,13 @@ function getData(name) {
 
 function correctionWithBaseColor() {
 	var canvas = document.getElementById("canvas_correct");
-
-	data_correct = getData("canvas_correct");
-	data_spoiled = getData("canvas_spoiled");
-
-	canvas.onmousedown = getPixelCanvas;
-
-	var cur_pos = (example_pixel[0] * canvas.height + example_pixel[j]) * 4;
-
-	r_dst = data_correct[cur_pos];
-	r_src = data_correct[cur_pos];
-	coeff_r = r_dst / r_src;
-
-	g_dst = data_correct[cur_pos + 1];
-	g_src = data_correct[cur_pos + 1];
-	coeff_g = g_dst / g_src;
-
-	b_dst = data_correct[cur_pos + 2];
-	b_src = data_correct[cur_pos + 2];
-	coeff_b = b_dst / b_src;
-
-	for (var i = 0; i < width; ++i) {
-		for (var j = 0; j < height; ++j) {
-			cur = (i * height + j) * 4;
-			data[cur] *= coeff_r;
-			data[cur + 1] *= coeff_g;
-			data[cur + 2] *= coeff_b;
-		}
-	}
-
-	
-	ctx = canvas.getContext('2d');
-	ctx.putImageData(data_spoiled, 0, 0);
-
+	canvas.addEventListener('mousedown', getPixelCanvas);
 }
-// removeEventListener
+
+function removeEvents() {
+	var canvas_correct = document.getElementById("canvas_correct");
+	canvas_correct.removeEventListener('mousedown', getPixelCanvas);
+}
 
 function init() {
 	canvasInit("canvas_spoiled");
